46cf5227bbf4191cf862602baf4160cf4d4f69e9
Vincent Willcox <talking@talktech.info>
2025-02-08 18:20:46 +0000

    Update main.py
    Rewrite to use Presto's Vector mode.
    Catch more errors
    overall less crasy!

--- main.py
+++ main.py
@@ -3,69 +3,86 @@ from spotify_web_api import (
     SpotifyWebApiError,
 )
 
+import datetime
 import time
-import machine
-##import st7789
+
+from picovector import ANTIALIAS_BEST, PicoVector, Polygon, Transform
 from presto import Presto
 from touch import Button
 
-#Connect to Spotify using the library
-presto = Presto(ambient_light=True)
+presto = Presto(ambient_light=True, full_res=True)
 display = presto.display
-touch = presto.touch
-
-WIDTH, HEIGHT = display.get_bounds()
-
-#connect to WIFI using secrets.py
 connection_successful = presto.connect()
-print(connection_successful)
 spotify = spotify_client()
 
 WIDTH, HEIGHT = display.get_bounds()
-BLUE = display.create_pen(28, 181, 202)
-WHITE = display.create_pen(255, 255, 255)
 
+CX = WIDTH // 2
+CY = HEIGHT // 2
+
+# Couple of colours for use later
+WHITE = display.create_pen(255, 255, 255)
 RED = display.create_pen(230, 60, 45)
-ORANGE = display.create_pen(245, 165, 4)
 GREEN = display.create_pen(9, 185, 120)
-PINK = display.create_pen(250, 125, 180)
-PURPLE = display.create_pen(118, 95, 210)
 BLACK = display.create_pen(0, 0, 0)
 
-button_1 = Button(0, HEIGHT-50, 100, 50)
+# We'll need this for the touch element of the screen
+touch = presto.touch
 
-pen = display.create_pen_hsv(1.0, 1.0, 1.0)
+# Pico Vector
+vector = PicoVector(display)
+vector.set_antialiasing(ANTIALIAS_BEST)
+t = Transform()
 
-display.set_pen(BLACK)
-display.clear()
-presto.update()
+vector.set_font("Roboto-Medium.af", 54)
+vector.set_font_letter_spacing(100)
+vector.set_font_word_spacing(100)
+vector.set_transform(t)
+
+# Touch buttons
+start_button = Button(1, HEIGHT - 50, CX - 2, 49)
+
+start = Polygon()
+start.rectangle(*start_button.bounds, (5, 5, 5, 5))
+
+outline = Polygon()
+outline.rectangle(5, 20, WIDTH - 10, HEIGHT - 100, (5, 5, 5, 5), 2)
+
+def truncate_string(input_string):
+    if len(input_string) > 38:
+        return input_string[:38]
+    else:
+        return(input_string)
+    return 
 
 while True:
-    touch.poll()
-    now_playing = spotify.get_currently_playing
-    current_track = now_playing()
-    is_playing = current_track['is_playing']
-    if is_playing is True:
-        display.set_pen(BLUE)
-        display.clear()
+    try:
+        now_playing = spotify.get_currently_playing
+        current_track = now_playing()
+        is_playing = current_track['is_playing']
+        display.set_pen(BLACK)
+        display.clear()    
+        display.set_pen(GREEN)
+        vector.draw(start)
         display.set_pen(WHITE)
-        display.text(current_track['item']['artists'][0]['name'], 8, 10, WIDTH, 4)
-        display.text(current_track['item']['name'], 10, 85, WIDTH, 4)
-        if button_1.is_pressed():
-            display.set_pen(RED)
-            print("pausing")
-            #display.text("Pressed", 10, HEIGHT-20, 100, 2)
-            spotify.pause()
-            #time.sleep(0.05)
-            #while button_1.is_pressed():
-            #    touch.poll()
+        vector.draw(outline)
+        vector.set_font_size(32)
+    #vector.text("Pause", start_button.bounds[0] +83, start_button.bounds[1] + 33)
+        if is_playing is True:
+            vector.text(current_track['item']['artists'][0]['name'], 10, 50)
+            cur_track = (current_track['item']['name'])
+            display_track = truncate_string(str(cur_track))
+            vector.text(str(display_track), 10, 80)
+            vector.text("Pause", start_button.bounds[0] +83, start_button.bounds[1] + 33)
+            if start_button.is_pressed():
+                spotify.pause()
         else:
-            display.set_pen(GREEN)
-        display.rectangle(*button_1.bounds)
-    else:
-        #display.set_pen(BLACK)
-        if button_1.is_pressed():
-            print("playing")
-            spotify.play()
-    presto.update()
-    time.sleep(0.05)
+            vector.text("Play", start_button.bounds[0] +83, start_button.bounds[1] + 33)
+            if start_button.is_pressed():
+                spotify.play()
+        presto.update()
+    except Exception as e:
+        print(e)
+    
+
+
