719363520bd22ac2d9b9314ddf608c714aad7b51
Vincent Willcox <talking@talktech.info>
2025-02-14 23:22:26 +0000

    Update main.py - displays album artwork
    Now shows the current playing tracks album artwork

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from spotify_web_api import (
 
 import datetime
 import time
+import sdcard
+import uos
+import machine
+import urequests
 
 from picovector import ANTIALIAS_BEST, PicoVector, Polygon, Transform
 from presto import Presto
 from touch import Button
+import jpegdec
 
 presto = Presto(ambient_light=True, full_res=True)
 display = presto.display
@@ -52,6 +57,8 @@ skip.rectangle(*skip_button.bounds, (5, 5, 5, 5))
 
 outline = Polygon()
 outline.rectangle(5, 20, WIDTH - 10, HEIGHT - 100, (5, 5, 5, 5), 2)
+j = jpegdec.JPEG(display)
+
 
 def truncate_string(input_string):
     if len(input_string) > 36:
@@ -59,6 +66,16 @@ def truncate_string(input_string):
     else:
         return(input_string)
     return 
+
+try:
+    # Setup for SD Card
+    sd_spi = machine.SPI(0, sck=machine.Pin(34, machine.Pin.OUT), mosi=machine.Pin(35, machine.Pin.OUT), miso=machine.Pin(36, machine.Pin.OUT))
+    sd = sdcard.SDCard(sd_spi, machine.Pin(39))
+
+    # Mount the SD to the directory 'sd'
+    uos.mount(sd, "/sd")
+except OSError as e:
+    print(e)
 
 while True:
     try:
@@ -77,9 +94,17 @@ while True:
         vector.set_font_size(32)
     #vector.text("Pause", start_button.bounds[0] +83, start_button.bounds[1] + 33)
         if is_playing is True:
+            #print(current_track)
             vector.text(current_track['item']['artists'][0]['name'], 10, 50)
             cur_track = (current_track['item']['name'])
             display_track = truncate_string(str(cur_track))
+            #artwork = current_track['items'][1]['album']['images'][0]['url']
+            artworks = [image['url'] for image in current_track['item']['album']['images']]
+            #print(artworks[1])
+            response2 = urequests.get(artworks[1])
+            image_data = response2.content
+            j.open_file(image_data)
+            j.decode(100, 90, jpegdec.JPEG_SCALE_FULL, dither=True)
             vector.text(str(display_track), 10, 80)
             vector.text("Pause", start_button.bounds[0] +83, start_button.bounds[1] + 33)
             vector.text("Skip", skip_button.bounds[0] + 83, skip_button.bounds[1] + 33)
